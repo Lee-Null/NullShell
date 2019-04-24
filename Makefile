5080162b76100cd6ecfcbc8ec6269e87a549039e
Lee-Null <nullist.null@gmail.com>
2019-04-25 03:44:34 +0900

    refs #2 - standard file

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 CC = gcc
 
-shell : interpret.o
+all : shell
+
+shell : interpret.o shell.c
 	$(CC) -o shell shell.c interpret.o
 
-interpret.o :
+interpret.o : interpret.c interpret.h
 	$(CC) -c -o interpret.o interpret.c
 
 clean:
--- interpret.c
+++ interpret.c
@@ -26,27 +26,78 @@ int pipeline(int *mod, char *tok){
 }
 
 int exec(char **cmd, int into, int from){
-    if(fork() == 0){
-        dup2(0, from);
-        dup2(1, into);
+    char *c; int i, pid, status = 0;
+    printf("exec stdin : %d, stdout : %d\n", from, into);
+
+        for(i = 0, c = cmd[i]; c != NULL; c = cmd[++i])
+            printf("%s ", c);
+        printf("\n");
+
+    if((pid = fork()) == 0){
+        dup2(from, STDIN_FILENO); //stdin
+        dup2(into, STDOUT_FILENO); //stdout
+
         execvp(cmd[0], cmd);
         execv(cmd[0], cmd);
         printf("No such command\n");
-        exit(1);
+        exit(0);
     }
     else{
-        wait(NULL);
+        waitpid(pid, &status, 0);
+        printf("after stdin : %d, stdout : %d\n", from, into);
+        if(into != STDOUT_FILENO)
+            close(into);
+        if(from != STDIN_FILENO)
+            close(from);
     }
 }
 
 int interpret(int *mod, char *cmds, char *dir){
     char *tok[INPUT_SIZE/TOK_SIZE];
-    char *cache;
+    char *cache, *file;
     int tok_count = 0, input_fd = 0, output_fd = 1;
     int flag = -1;
 
     printf("TOKKEN : ");
     cache = strtok(cmds, " ");
+    while(cache != NULL){
+        switch (flag = pipeline(mod, cache)){
+            case then:
+                //pipelne in |
+                break;
+            case into:
+                file = strtok(NULL, " ");
+                if(file != NULL)
+                    output_fd = open(file, O_CREAT | O_RDWR | O_TRUNC, 0664);
+                if(file == NULL || output_fd < 0){
+                    fprintf(stderr, "File cannot exist\n");
+                    return -1;
+                }
+                break;
+            case from:
+                file = strtok(NULL, " ");
+                if(file != NULL)
+                    input_fd = open(file, O_RDONLY);
+                if(file == NULL || input_fd < 0){
+                    fprintf(stderr, "File does not exist\n");
+                    return -1;
+                }
+                break;
+            case next:
+                tok[tok_count++] = NULL;
+                exec(tok, output_fd, input_fd);
+                input_fd = STDIN_FILENO; output_fd = STDOUT_FILENO; tok_count = 0;
+                break;
+            default:
+                break;
+        }
+        if(flag < 0)
+            tok[tok_count++] = cache;
+
+        cache = strtok(NULL, " ");
+    }
+
+    /*
     do {
         printf("%s ", cache);
         switch (flag){
@@ -54,15 +105,19 @@ int interpret(int *mod, char *cmds, char *dir){
                 
                 break;
             case into:
-                input_fd = open(cache, O_CREAT | O_WRONLY);
+                output_fd = open(cache, O_CREAT | O_RDWR | O_TRUNC, 0664);
+                continue;
                 break;
             case from:
-                output_fd = open(cache, O_RDONLY);
+                input_fd = open(cache, O_RDONLY);
+                if(input_fd < 0)
+                    input_fd = 0;
+                continue;
                 break;
             case next:
                 tok[tok_count++] = NULL;
-                exec(tok, input_fd, output_fd);
-                input_fd = 0; output_fd = 1; tok_count = 0;
+                exec(tok, output_fd, input_fd);
+                input_fd = STDIN_FILENO; output_fd = STDOUT_FILENO; tok_count = 0;
                 break;
             default:
                 break;
@@ -74,5 +129,6 @@ int interpret(int *mod, char *cmds, char *dir){
             tok[tok_count++] = cache;
         
     } while((cache = strtok(NULL, " ")) != NULL);
+    */
     printf("TOKKEN COUNT : %d\n", tok_count);
 }
